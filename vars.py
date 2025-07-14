66412742a17ae0e4d18ca8b1ba867b4a633af620
yourfriend143 <ramukakayourdada143@gmail.com>
2025-07-14 13:54:28 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,14 +3,14 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "xxxxxxxxx"))
-API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxxxxx")
+API_ID = int(environ.get("API_ID", "23283708"))
+API_HASH = environ.get("API_HASH", "7805011fb84729023531f0fa3f000bec")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "5680454765"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
+OWNER = int(environ.get("OWNER", "6481888008"))
+CREDIT = environ.get("CREDIT", "🅖🅤🅜🅝🅐🅐🅜")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '6481888008').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
 AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
